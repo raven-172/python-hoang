0fe79e0967e05fb12a25c17f30bc19bd308bb986
LE VIET HUY HOANG <leviethuynhoang172@gmail.com>
2025-07-25 17:58:59 +0700

    TH MART

--- thmart.project/hoang.sql
+++ thmart.project/hoang.sql
@@ -1,1 +1,18 @@
-CREATE 
+CREATE TABLE store_list(
+    store_id NUMERIC PRIMARY KEY,
+    store_name VARCHAR(50)
+);
+
+INSERT INTO store_list (store_id, store_name)
+VALUES 
+    (1, 'TH Mart 1'),
+    (2, 'TH Mart 2'),
+    (3, 'TH Mart 3');
+
+CREATE TABLE sales(
+    sale_id VARCHAR(50) PRIMARY KEY,
+    sale_date TIMESTAMP,
+    store_id NUMERIC,
+    FOREIGN KEY (store_id) REFERENCES store_list(store_id)
+);
+
