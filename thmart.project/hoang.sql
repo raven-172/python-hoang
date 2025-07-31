eb0b12a78f68be5a7c8212f929706d120f2434ee
LE VIET HUY HOANG <leviethuynhoang172@gmail.com>
2025-07-31 14:17:21 +0700

    thmart

--- thmart.project/hoang.sql
+++ thmart.project/hoang.sql
@@ -11,12 +11,31 @@ VALUES
     (4, 'TH Mart 4')
     (5, 'TH Mart 5');
 
-CREATE TABLE sales(
-    sale_id VARCHAR(50) PRIMARY KEY,
-    sale_date TIMESTAMP,
+CREATE TABLE returns_list(
     store_id NUMERIC,
+    return_id VARCHAR (50) PRIMARY KEY,
+    return_date TIMESTAMP,
+    return_value NUMERIC(10,2),
     FOREIGN KEY (store_id) REFERENCES store_list(store_id)
 );
 
+COPY returns_list(store_id, return_id, return_date,return_value)
+FROM 'C:/thmart/return_list.csv'
+DELIMITER ',' CSV HEADER;
 
+SELECT * FROM returns_list;
 
+CREATE TABLE invoices_list(
+    store_id NUMERIC,
+    invoice_id VARCHAR(50) PRIMARY KEY,
+    invoice_date TIMESTAMP,
+    return_id VARCHAR (50),
+    invoice_value NUMERIC(10,2),
+    invoice_discount NUMERIC(10,2)
+);
+
+COPY returns_list(store_id, return_id, return_date,return_value)
+FROM 'C:/thmart/return_list.csv'
+DELIMITER ',' CSV HEADER;
+
+SELECT * FROM invoices_list;
