a1e6b16b8275750a5c020bd4a422bab0adb42ad4
LE VIET HUY HOANG <leviethuynhoang172@gmail.com>
2025-07-31 15:58:20 +0700

    thmart

--- thmart.project/hoang.sql
+++ thmart.project/hoang.sql
@@ -1,8 +1,10 @@
+/*Create table about STORES*/
 CREATE TABLE store_list(
     store_id NUMERIC PRIMARY KEY,
     store_name VARCHAR(50)
 );
 
+/*Import data presented information about STORES*/
 INSERT INTO store_list (store_id, store_name)
 VALUES 
     (1, 'TH Mart 1'),
@@ -11,6 +13,10 @@ VALUES
     (4, 'TH Mart 4')
     (5, 'TH Mart 5');
 
+/*Check result*/
+SELECT* FROM store_list;
+
+/*Create table about STORES'S RETURNS*/
 CREATE TABLE returns_list(
     store_id NUMERIC,
     return_id VARCHAR (50) PRIMARY KEY,
@@ -19,12 +25,15 @@ CREATE TABLE returns_list(
     FOREIGN KEY (store_id) REFERENCES store_list(store_id)
 );
 
+/*Import data presented information about STORES'S RETURNS*/
 COPY returns_list(store_id, return_id, return_date,return_value)
 FROM 'C:/thmart/return_list.csv'
 DELIMITER ',' CSV HEADER;
 
+/*Check result*/
 SELECT * FROM returns_list;
 
+/*Create table about STORES'S INVOICES*/
 CREATE TABLE invoices_list(
     store_id NUMERIC,
     invoice_id VARCHAR(50) PRIMARY KEY,
@@ -34,8 +43,32 @@ CREATE TABLE invoices_list(
     invoice_discount NUMERIC(10,2)
 );
 
-COPY returns_list(store_id, return_id, return_date,return_value)
-FROM 'C:/thmart/return_list.csv'
+/*Import data presented information about  STORES'S INVOICES*/
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_1.csv'
 DELIMITER ',' CSV HEADER;
 
-SELECT * FROM invoices_list;
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_2.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_3.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_4.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_5.csv'
+DELIMITER ',' CSV HEADER;
+
+/*Update data (from 2017 to 2019) presented information about  STORE 1'S INVOICES*/
+COPY invoices_list(store_id,invoice_id,invoice_date,return_id,invoice_value,invoice_discount)
+FROM 'C:/thmart/sale_list/sale_list_add.csv'
+DELIMITER ',' CSV HEADER;
+
+/*Check result*/
+SELECT * FROM invoices_list
+ORDER BY invoice_date;
