216a564958cd7cdff8c0a3a0eb0d01b787604277
LE VIET HUY HOANG <leviethuynhoang172@gmail.com>
2025-08-01 14:02:21 +0700

    thmart

--- thmart.project/hoang.sql
+++ thmart.project/hoang.sql
@@ -70,17 +70,33 @@ FROM 'C:/thmart/sale_list/sale_list_add.csv'
 DELIMITER ',' CSV HEADER;
 
 /*Check result*/
-SELECT * FROM invoices_list
-ORDER BY invoice_date;
 
-SELECT
-    s.store_name, 
-    EXTRACT(YEAR FROM i.invoice_date) AS invoice_year,
-    EXTRACT(MONTH FROM i.invoice_date) AS invoice_month,
-    ROUND(AVG(EXTRACT(EPOCH FROM MAX(i.invoice_date) - MIN(i.invoice_date))/3600),2) AS avg_hours_per_day,
-    COUNT(*)/EXTRACT(EPOCH FROM MAX(i.invoice_date) - MIN(i.invoice_date)/3600) AS invoice_per_hour,
-
-
-/*Create table with all insight relate with sale*/
-CREATE TABLE report AS
+WITH 
+daily_report AS (
     SELECT 
+        s.store_name,
+        DATE(i.invoice_date) AS sale_day,
+        COALESCE(EXTRACT(EPOCH FROM MAX(i.invoice_date) - MIN(i.invoice_date))/3600, 0) AS total_hours_day,
+        COUNT(*) AS amount_invoice,
+        COALESCE(COUNT(*)/NULLIF(EXTRACT(EPOCH FROM MAX(i.invoice_date)-MIN(i.invoice_date))/3600,0),0) AS invoice_per_hour,
+        SUM(i.invoice_value) - COALESCE(SUM(r.return_value),0) AS net_income,
+        SUM(i.invoice_value) - COALESCE(SUM(r.return_value),0)/COUNT(*) AS average_invoice_value,
+        COALESCE(SUM(i.invoice_discount),0) AS discount_per_day,
+        SUM(i.invoice_value) AS income_per_day
+    FROM invoices_list i
+    LEFT JOIN  store_list s ON s.store_id = i.store_id
+    LEFT JOIN returns_list r ON r.return_id = i.return_id
+    GROUP BY s.store_name, sale_day
+)
+
+    SELECT
+        store_name,
+        EXTRACT(YEAR FROM sale_day) AS invoice_year,
+        EXTRACT(MONTH FROM sale_day) AS invoice_month,
+        ROUND(AVG(total_hours_day),2) AS operating_time,
+        ROUND(AVG(invoice_per_hour),2) AS avg_inv_per_hour,
+        SUM(net_income) AS total_net_income,
+        SUM(net_income)/SUM(amount_invoice) AS avg_invoice_value,
+        ROUND(SUM(discount_per_day)/SUM(income_per_day),2) AS discount_income_rate
+    FROM daily_report
+    GROUP BY invoice_year, invoice_month, daily_report.store_name;
